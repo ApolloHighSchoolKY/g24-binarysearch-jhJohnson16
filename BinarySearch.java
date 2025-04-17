844a109da4e895e85e975eefbde7421c06cf378d
jhJohnson16 <jacob.johnson4@stu.daviess.kyschools.us>
2025-04-17 03:54:23 +0000

    giggidy giggidy goo

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,34 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
+		Arrays.sort(ray);
 
+		int bottom = 0;
+		int top = ray.length-1;
+		int middle = 0;
+
+		while(bottom <= top)
+		{
+			middle = (bottom + top)/2;
+
+			if(ray[middle] > ray[item]){
+				top = middle-1;
+			}
+			else if(ray[middle] < item)
+			{
+				bottom = middle+1;
+			}
+			else
+			{
+				return middle;
+			}
+		}
+		if(ray[middle] != item)
+		{
+			return -1;
+		}
+		else
+		return middle;
 	}
 }
+
